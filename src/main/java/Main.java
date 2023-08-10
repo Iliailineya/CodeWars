47f5c7ff78fb2f7b1fed2a4db028d6e05cc136e5
Iliailineya <ilavituk011@gmail.com>
2023-08-10 17:23:16 +0300

    Do better

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-package _6_kyu;
+import _6_kyu.NumberUtils;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(NumberUtils.isNarcissistic(153));
-        System.out.println( String.valueOf(153));
+        System.out.println(153);
     }
 }
