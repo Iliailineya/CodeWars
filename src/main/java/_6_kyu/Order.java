23aec4f70032563c2fd03fd0c812b8ab4dc6242b
Iliailineya <ilavituk011@gmail.com>
2023-08-11 16:02:37 +0300

    Order final attempt (success)

--- src/main/java/_6_kyu/Order.java
+++ src/main/java/_6_kyu/Order.java
@@ -1,14 +1,17 @@
 package _6_kyu;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 /*
-Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+Your task is to sort a given string. Each word in the string will contain a single number.
+This number is the position the word should have in the result.
 
 Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
 
-If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+If the input string is empty, return an empty string.
+The words in the input String will only contain valid consecutive numbers.
 
 Examples
 "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
@@ -18,13 +21,10 @@ Examples
 public class Order {
     public static String order(String str) {
         return Arrays.stream(str.split(" "))
+//                Разделяем строку на массив слов по " "
                 .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
+//                Сортируем массив используя в качестве аргумента компаратора значение, полученое путем удаления всех
+//                символов, не являющимися числами из слова
                 .collect(Collectors.joining(" "));
-    }
-
-    public static void main(String[] args) {
-        String input = "is2 Thi1s T4est 3a";
-        String ordered = order(input);
-        System.out.println(ordered);
     }
 }
