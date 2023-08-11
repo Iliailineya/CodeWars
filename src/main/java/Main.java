9694bf99b935f7d8267336916d7fd202e3a3e4ac
Iliailineya <ilavituk011@gmail.com>
2023-08-11 14:16:35 +0300

    Do better

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import _6_kyu.NumberUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int ii = '0';
+        System.out.println(ii);
         System.out.println(NumberUtils.isNarcissistic(153));
         System.out.println(153);
     }
