95aa09446a0e8b2f1f3381a9f6a0c4f198012b51
Iliailineya <ilavituk011@gmail.com>
2023-08-11 15:19:05 +0300

    Order first attempt (wrong)

---
+++ src/main/java/_6_kyu/Order.java
@@ -0,0 +1,35 @@
+package _6_kyu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+public class Order {
+    public static String order(String str) {
+        String[] words = str.split(" ");
+        List<String> result = new ArrayList<>(words.length);
+        for(String word : words){
+            int i =0;
+            String[] tt = word.split("");
+            for(String letter : tt){
+                if (letter.equals("[1-9]")){
+                    i = Integer.parseInt(letter);
+                }
+            }
+            result.set(i - 1, word);
+        }
+
+        return result.toString();
+    }
+}
