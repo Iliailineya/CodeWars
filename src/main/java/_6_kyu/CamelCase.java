3558dbbc147b8a3a658728fab98b86a55409a121
Iliailineya <ilavituk011@gmail.com>
2023-08-15 14:59:02 +0300

    CamelCase first attempt (success)

--- src/main/java/_6_kyu/CamelCase.java
+++ src/main/java/_6_kyu/CamelCase.java
@@ -1,8 +1,36 @@
 package _6_kyu;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+ */
 public class CamelCase {
 
+    public static void main(String[] args) {
+        String input = "hello_world_example";
+        String camelCaseString = toCamelCase(input);
+        System.out.println(camelCaseString); // Output: helloWorldExample
+    }
+
+//                .skip(1)
     public static String toCamelCase(String s){
-        return "TheStealthWarrior";
+        String[] words = s.split("[^A-Za-z]");
+
+        String firstWord = words.length > 0 ? words[0] : "";
+        String restOfWords = Arrays.stream(words)
+                .skip(1)
+                .map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
+                .collect(Collectors.joining());
+
+        return firstWord + restOfWords;
     }
 }
